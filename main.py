a0b1b56aae5c24c8a5d80c80113bc44a2882ed8c
Sebastián Quiroga <50588970+squirogar@users.noreply.github.com>
2023-05-20 20:29:33 -0400

    loading screen and the result window added

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import tkinter as tk
+from tkinter import ttk
 import tkinter.filedialog as fd
 from PIL import ImageTk, Image
 from tkinter import messagebox
+import Kmeans as km
+import numpy as np
+
 
 class Aplicacion(tk.Tk):
     
@@ -11,6 +15,7 @@ class Aplicacion(tk.Tk):
         self.resizable(True, True)  
         self.__imagen = None
         self.__ventanaConf = None
+        self.__listaHiperparametros = []
 
         # menu #
         self.__myMenu = self.__creaMenu()
@@ -19,24 +24,44 @@ class Aplicacion(tk.Tk):
         # open file #
         tk.Button(self, text="Abrir archivo", command=self.__openFile).pack()
         self.__imagenLabel = tk.Label(self, image=self.__imagen)
-        self.__imagenLabel.pack()
+        
 
         # run k-means #
         tk.Button(self, text="Run K-means", command=self.__comprobarHiperparametros).pack()
+        self.__imagenLabel.pack()
+
+        
 
 
     def __comprobarHiperparametros(self):
-        
         #si la lista de hiperparametros esta completa y la imagen esta cargada se ejecuta k-means
-        #si una de las condiciones anteriores no se cumplio entonces retornar un mensaje de error
-        messagebox.showerror(
-            title="Error", 
-            message=("No es posible ejecutar K-means. Revise que se cargó la imagen correctamente "
-            "y configuró adecuadamente los hiperparámetros en el menú Herramientas -> Configuración.")
-        )    
+        if self.__imagen != None and not self.__listaHiperparametros:
+            proceso = LoadingScreen(self, self.__imagen, self.__listaHiperparametros)
+            imagenComprimida = proceso.getImagen()
+            proceso.destroy()
+            ventanaImagen = VentanaResultado(self, imagenComprimida)
+            
+        else:
+            #si una de las condiciones anteriores no se cumplio entonces retornar un mensaje de error
+            messagebox.showerror(
+                title="Error", 
+                message=("No es posible ejecutar K-means. Revise que se cargó la imagen correctamente "
+                "y configuró adecuadamente los hiperparámetros en el menú Herramientas -> Configuración.")
+            )
 
+
+    def clearListaHiperparametros(self):
+        self.__listaHiperparametros.clear()
+
+    def setListaHiperparametros(self, params):
+        self.__listaHiperparametros.extend(params)
+        print(self.__listaHiperparametros)
 
     def __creaMenu(self):
+        """
+        Retorna una barra de menú con las opciones de "archivo", "herramientas", "ayuda" y "acerca de"
+
+        """
         newMenu = tk.Menu(self)
 
         file = tk.Menu(self, tearoff=0)
@@ -44,17 +69,16 @@ class Aplicacion(tk.Tk):
         help = tk.Menu(self, tearoff=0)
         about = tk.Menu(self, tearoff=0)
 
-        file.add_command(label="Abrir archivo")
-        file.add_command(label="Cerrar archivo")
-        file.add_separator()
-        file.add_command(label="Salir")
+        file.add_command(label="Salir", command=self.__salida)
 
+        tools.add_command(label="Limpiar imagen", command=self.__clearImg)
+        tools.add_separator()
         tools.add_command(label="Configuración", command=self.__showConfig)
 
-        help.add_command(label="Guía de uso")
+        help.add_command(label="Instrucciones de uso", command=self.__showInstruc)
 
-        about.add_command(label="Licencia")
-        about.add_command(label="Autor")
+        about.add_command(label="Licencia", command=self.__showLicencia)
+        about.add_command(label="Autor", command=self.__showAutor)
 
         newMenu.add_cascade(label="Archivo", menu=file)
         newMenu.add_cascade(label="Herramientas", menu=tools)
@@ -69,6 +93,8 @@ class Aplicacion(tk.Tk):
             fichero = fd.askopenfilename(title = "Abrir imagen", initialdir = "./", filetypes = (("Imágenes png", "*.png"), ("Imágenes jpg", "*.jpg")))
             print(fichero)
             imagen = ImageTk.PhotoImage(Image.open(fichero))
+            
+            
         except AttributeError as ae:
             print("Error al cargar el archivo")
         else:
@@ -85,9 +111,37 @@ class Aplicacion(tk.Tk):
             print("destroyed")
         self.__ventanaConf = Configuracion(self)
     
+    def __clearImg(self):
+        self.__imagen = None
+
+    def __salida(self):
+        self.destroy()
     
-        
-        
+    def __showInstruc(self):
+        messagebox.showinfo(
+            title="Instrucciones",
+            message=("1. Cargue la imagen presionando el boton 'Abrir' y seleccione la imagen correspondiente\n"
+            "2. Establezca los hiperparámetros de K-means en el menú 'Herramientas' > 'Configuración'\n"
+            "3. Presione el botón 'Run K-means' para ejecutar el algoritmo K-means. El algoritmo puede tardarse"
+            " dependiendo de los cálculos que hace el algoritmo, por favor espere a que termine.\n"
+            "4. Una vez terminado el algoritmo se mostrará en una nueva ventana la imagen comprimida. En el"
+            " menú 'opciones' puede guardar la imagen en su PC.")
+        )
+    
+    def __showLicencia(self):
+        messagebox.showinfo(
+            title="Licencia",
+            message="Programa de libre uso y de código abierto."
+        )
+
+    def __showAutor(self):
+        messagebox.showinfo(
+            title="Autor",
+            message="Programa creado por Sebastián Quiroga R."
+        )
+
+
+
 
 
 class Configuracion(tk.Toplevel):
@@ -110,10 +164,8 @@ class Configuracion(tk.Toplevel):
         self.__verbose = tk.IntVar()
         RBNo = tk.Radiobutton(self, text="No", variable=self.__verbose, value=0)
         RBYes = tk.Radiobutton(self, text="Yes", variable=self.__verbose, value=1)
-        buttonGuardar = tk.Button(self, text="Guardar", command=self.__validaCampos)
+        buttonGuardar = tk.Button(self, text="Guardar", command=lambda:self.__validaCampos(root))
 
-
-        self.__listaHiperparametros = None
 
         labelClusters.pack()
         self.__numberClusters.pack()
@@ -129,7 +181,7 @@ class Configuracion(tk.Toplevel):
             return True
         return False
 
-    def __validaCampos(self):
+    def __validaCampos(self, root):
         print("verbose: ", self.__verbose.get())
         flag = False
         if self.__numberClusters.get() == "" or self.__numIters.get() == "":
@@ -138,18 +190,60 @@ class Configuracion(tk.Toplevel):
         if flag:
             messagebox.showerror(title="Error", message="Uno o más campos están vacíos")
         else:
-            if self.__listaHiperparametros is not None:
-                del self.__listaHiperparametros
-            self.__listaHiperparametros = []
+
+            root.clearListaHiperparametros()
             
-            self.__listaHiperparametros.extend(
-                self.__numberClusters.get(),
-                self.__numIters.get(),
-                self.__verbose.get()
+            root.setListaHiperparametros((
+                int(self.__numberClusters.get()),
+                int(self.__numIters.get()),
+                int(self.__verbose.get())
+            ))
+
+            messagebox.showinfo(
+                title="Aviso",
+                message="Configuración guardada correctamente."
             )
-    
-    #def getListaHiperparametros(self):
-    #    return self.__listaHiperparametros
+
+            self.destroy()
+
+
+
+
+
+
+class LoadingScreen(tk.Toplevel):
+    def __init__(self, root, imagen, hiperparametros):
+        super().__init__(root)
+        pb = ttk.Progressbar(
+            self, 
+            orient="horizontal", 
+            mode="indeterminate", 
+            length="300")
+        
+        mensaje = tk.Label(
+            self, 
+            text="Algoritmo de clustering K-means ejecutandose, por favor espere..."
+        )
+
+        pb.pack()
+        mensaje.pack()
+
+        self.__run_algrithm(imagen, hiperparametros)
+        
+
+
+
+    def __run_algorithm(self, imagen_original, hiperparametros):
+        num_clusters, num_ejecuciones, num_iter = hiperparametros
+        # hiperparametro es una lista 
+        # se llama a fit()
+        model = km.KMeans(num_clusters, num_ejecuciones, num_iter)
+        _, centroids, index_centroids = model.fit(imagen_original)
+        # fit retorna costo, centroids, index_centroids
+        imagen = centroids[index_centroids]
+        imagen = np.reshape(imagen, imagen_original.shape)
+
+        pass
 
 
 class VentanaResultado(tk.Toplevel):
@@ -164,8 +258,9 @@ class VentanaResultado(tk.Toplevel):
 
 
 
-class LoadingScreen(tk.Toplevel):
-    pass
+        
+
+
 
 
 
